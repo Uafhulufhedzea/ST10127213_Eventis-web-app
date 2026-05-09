fc9791afc27f7e510187ce8df935e8b16af254da
Rambuda <Rambuda@Bobs-MacBook-Pro.local>
2026-05-09 04:01:46 +0200

    Remove temporary debug error page - all features tested and working

--- Eventis web app/Program.cs
+++ Eventis web app/Program.cs
@@ -20,17 +20,11 @@ builder.Services.AddScoped<BlobStorageService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// Temporarily show detailed errors for debugging
-app.UseDeveloperExceptionPage();
-
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
